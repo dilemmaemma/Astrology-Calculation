778e2886c74cfa2294e02d5697d9b5ca334e8648
delimmaemma <calvinhtml@gmail.com>
2024-01-02 14:47:39 -0600

    Fixed issues. Calculation works

--- utils/geolocation_utils.py
+++ utils/geolocation_utils.py
@@ -11,11 +11,39 @@ def get_lat_long(location):
 
             # Validate and parse birth date
             while True:
+                months = [
+                    'January', 
+                    'February', 
+                    'March', 
+                    'April', 
+                    'May', 
+                    'June', 
+                    'July', 
+                    'August', 
+                    'September', 
+                    'October', 
+                    'November', 
+                    'December'
+                ]
                 try:
-                    date = input("Birth Date (Use two digit month, two digit day, and four digit year separated by '-'): ")
-                    date_parts = [int(part) for part in date.split('-')]
-                    if len(date_parts) == 3 and 1 <= date_parts[0] <= 12 and 1 <= date_parts[1] <= 31:
+                    month = input("Birth Month: ")
+                    if month in months:
+                        month = months.index(month) + 1
+                    else:
+                        month = int(month)
+                    if len(str(month)) != 2:
+                        month = str(month).rjust(2, '0')
+                    day = input("Birth Day: ")
+                    if len(str(day)) != 2:
+                        day = str(day).rjust(2, '0')
+                    year = input("Birth Year: ")
+                    if 1 <= int(month) <= 12 and 1 <= int(day) <= 31:
+                        date = f"{year}-{month}-{day}"
                         break
+                    # date = input("Birth Date (Use two digit month, two digit day, and four digit year separated by '-'): ")
+                    # date_parts = [int(part) for part in date.split('-')]
+                    # if len(date_parts) == 3 and 1 <= date_parts[0] <= 12 and 1 <= date_parts[1] <= 31:
+                    #     break
                     else:
                         print("Invalid date format. Please try again.")
                 except ValueError:
@@ -34,7 +62,7 @@ def get_lat_long(location):
                         if tod.lower() == 'am' or tod.lower() == 'pm':
                             if tod.lower() == 'pm' and split_time[0] < 12:
                                 split_time[0] += 12
-                            time = f'{split_time[0]:02d}:{split_time[1]:02d}:00'
+                            time = f'{split_time[0]:02d}:{split_time[1]:02d}'
                             return date, time, latitude, longitude
                             # Add an else block here to handle invalid periods
                         else:
--- utils/natal_utils.py
+++ utils/natal_utils.py
@@ -1,89 +1,149 @@
 import ephem
+import json
 from datetime import datetime
+
+from immanuel.classes.serialize import ToJSON
+from immanuel import charts
+from immanuel.const import chart
+from immanuel.setup import settings
+
 from utils.astrology_utils import get_zodiac_sign, get_chinese_zodiac, calculate_zodiac_sign
-from utils.angles_utils import calculate_ascendant, calculate_descendant, calculate_midheaven, calculate_imum_coeli
+# from utils.angles_utils import calculate_ascendant, calculate_descendant, calculate_midheaven, calculate_imum_coeli
 from utils.terminal_helpers import clear_terminal
 
 # Calculate Natal Chart
 def generate_natal_chart(birth_date, birth_time, latitude, longitude):
+        
     birth_datetime_str = f"{birth_date} {birth_time}"
-    birth_datetime = datetime.strptime(birth_datetime_str, '%m-%d-%Y %H:%M:%S')
-
+    birth_datetime = datetime.strptime(birth_datetime_str, '%Y-%m-%d %H:%M')
+    
     observer = ephem.Observer()
     observer.lat, observer.lon = str(latitude), str(longitude)
     observer.date = birth_datetime
-
-    # Calculate and print planetary positions
-    planets = [
-        ephem.Sun(),
-        ephem.Moon(),
-        ephem.Mercury(),
-        ephem.Venus(),
-        ephem.Mars(),
-        ephem.Jupiter(),
-        ephem.Saturn(),
-        ephem.Uranus(),
-        ephem.Neptune(),
-        ephem.Pluto(),
-    ]
+    
+    native = charts.Subject(
+        datetime.fromisoformat(birth_datetime_str), 
+        f'{latitude:.2f}n' if latitude > 0 else f'{abs(latitude):.2f}s', 
+        f'{longitude:.2f}e' if longitude > 0 else f'{abs(longitude):.2f}w',
+    )
+    
+    settings.objects.append(chart.ASC)
+    settings.objects.append(chart.DESC)
+    settings.objects.append(chart.MC)
+    settings.objects.append(chart.IC)
+    settings.objects.append(chart.TRUE_NORTH_NODE)
+    settings.objects.append(chart.TRUE_SOUTH_NODE)
+    settings.objects.append(chart.CHIRON)
+    settings.objects.append(chart.LILITH)
+    settings.objects.append(chart.PHOLUS)
+    settings.objects.append(chart.CERES)
+    settings.objects.append(chart.PALLAS)
+    settings.objects.append(chart.JUNO)
+    settings.objects.append(chart.VESTA)
+    settings.objects.append(chart.VERTEX)
+    
+    natal = charts.Natal(native)
     
     clear_terminal()
-    # Calculate Planetary Positions
-    print("Planetary positions:")
-    for planet in planets:
-        planet.compute(observer)
+    
+    for object in natal.objects.values():
+        print(f'Daytime: {natal.diurnal}')
+        print(f'Moon phase: {natal.moon_phase}\n')
 
-        # Get the zodiac sign based on the degree
-        zodiac_sign = get_zodiac_sign(planet.az)
+        for object in natal.objects.values():
+            print(object)
+
+        print()
+
+        for index, aspects in natal.aspects.items():
+            print(f'Aspects for {natal.objects[index].name}:')
+
+            for aspect in aspects.values():
+                print(f' - {aspect}')
         
-        # Print the information
-        print(f"{planet.name}:")
-        print(f"   Sign: {zodiac_sign}")
-        print(f"   Degree: {planet.az}°")
-        print(f"   House: {calculate_zodiac_sign(planet.az - ascendant)}")
+    # Prints json format
+    # print(json.dumps(natal.objects, cls=ToJSON, indent=4))
 
-    # Calculate and print additional celestial bodies (if supported by ephem)
-    try:
-        additional_planets = [
-            ephem.North_Node(), ephem.South_Node(),
-            ephem.Lilith(), ephem.Juno(), ephem.Vesta(),
-            ephem.Ceres(), ephem.Pallas(), ephem.Chiron(), ephem.Eros()
-        ]
+# Calculate Natal Chart
+# def generate_natal_chart(birth_date, birth_time, latitude, longitude):
+#     birth_datetime_str = f"{birth_date} {birth_time}"
+#     birth_datetime = datetime.strptime(birth_datetime_str, '%m-%d-%Y %H:%M:%S')
 
-        for planet in additional_planets:
-            planet.compute(observer)
+#     observer = ephem.Observer()
+#     observer.lat, observer.lon = str(latitude), str(longitude)
+#     observer.date = birth_datetime
 
-            # Get the zodiac sign based on the degree
-            zodiac_sign = get_zodiac_sign(planet.az)
+#     # Calculate and print planetary positions
+#     planets = [
+#         ephem.Sun(),
+#         ephem.Moon(),
+#         ephem.Mercury(),
+#         ephem.Venus(),
+#         ephem.Mars(),
+#         ephem.Jupiter(),
+#         ephem.Saturn(),
+#         ephem.Uranus(),
+#         ephem.Neptune(),
+#         ephem.Pluto(),
+#     ]
+    
+#     clear_terminal()
+#     # Calculate Planetary Positions
+#     print("Planetary positions:")
+#     for planet in planets:
+#         planet.compute(observer)
+
+#         # Get the zodiac sign based on the degree
+#         zodiac_sign = get_zodiac_sign(planet.az)
+        
+#         # Print the information
+#         print(f"{planet.name}:")
+#         print(f"   Sign: {zodiac_sign}")
+#         print(f"   Degree: {planet.az}°")
+#         print(f"   House: {calculate_zodiac_sign(planet.az - ascendant)}")
+
+#     # Calculate and print additional celestial bodies (if supported by ephem)
+#     try:
+#         additional_planets = [
+#             ephem.North_Node(), ephem.South_Node(),
+#             ephem.Lilith(), ephem.Juno(), ephem.Vesta(),
+#             ephem.Ceres(), ephem.Pallas(), ephem.Chiron(), ephem.Eros()
+#         ]
+
+#         for planet in additional_planets:
+#             planet.compute(observer)
+
+#             # Get the zodiac sign based on the degree
+#             zodiac_sign = get_zodiac_sign(planet.az)
             
-            # Print the information
-            print(f"{planet.name}:")
-            print(f"   Sign: {zodiac_sign}")
-            print(f"   Degree: {planet.az}°")
-            print(f"   House: {calculate_zodiac_sign(planet.az - ascendant)}")
+#             # Print the information
+#             print(f"{planet.name}:")
+#             print(f"   Sign: {zodiac_sign}")
+#             print(f"   Degree: {planet.az}°")
+#             print(f"   House: {calculate_zodiac_sign(planet.az - ascendant)}")
 
-    except Exception as e:
-        print(f"Error: {e}")
-        print("Some celestial bodies are not supported by ephem.")
+#     except Exception as e:
+#         print(f"Error: {e}")
+#         print("Some celestial bodies are not supported by ephem.")
 
 
-    # Calculate AC, DC, MC, and IC
+#     # Calculate AC, DC, MC, and IC
     
-    # Calculate Ascendant (AC)
-    ascendant = calculate_ascendant(observer)
-    print(f"Ascendant: {ascendant}")
+#     # Calculate Ascendant (AC)
+#     ascendant = calculate_ascendant(observer)
+#     print(f"Ascendant: {ascendant}")
     
-    # Calculate Descendant (DC)
-    descendant = calculate_descendant(observer)
-    print(f"Descendant: {descendant}")
+#     # Calculate Descendant (DC)
+#     descendant = calculate_descendant(observer)
+#     print(f"Descendant: {descendant}")
 
-    # Calculate Midheaven (MC)
-    mc = calculate_midheaven(observer)
-    print(f"Midheaven (MC): {mc}")
+#     # Calculate Midheaven (MC)
+#     mc = calculate_midheaven(observer)
+#     print(f"Midheaven (MC): {mc}")
 
-    # Calculate Imum Coeli (IC)
-    ic = calculate_imum_coeli(observer)
-    print(f"Imum Coeli (IC): {ic}")
+#     # Calculate Imum Coeli (IC)
+#     ic = calculate_imum_coeli(observer)
+#     print(f"Imum Coeli (IC): {ic}")
 
     # Calculate and print Chinese zodiac sign
     # Use .triple() to get year, month, and day as a tuple
